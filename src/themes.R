f865f313bdeb55ada1eb62f901b268f164624f6e
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2020-11-25 09:39:51 -0700

    Add percent calculation

--- src/themes.R
+++ src/themes.R
@@ -11,31 +11,43 @@ disconnect <- read_csv("data/themes_csv/disconnect.csv")
 # PARTICIPANTS PER THEME --------------------------------------------------
 
 # Safety - number of participants endorsing this theme
-safety %>%
+saf_n <- safety %>%
   select(content) %>%
   filter(!is.na(content)) %>%
   nrow()
+saf_n
 
 # Connectedness - number of participants endorsing this theme
-connect %>%
+con_n <- connect %>%
   select(content) %>%
   filter(!is.na(content)) %>%
   nrow()
+con_n
 
 # Environmental Privilege - number of participants endorsing this theme
-environ %>%
+env_n <- environ %>%
   select(content) %>%
   filter(!is.na(content)) %>%
   nrow()
+env_n
 
 # Discrimination and Identity - number of participants endorsing this theme
-identity %>%
+ide_n <- identity %>%
   select(content) %>%
   filter(!is.na(content)) %>%
   nrow()
+ide_n
 
 # Disconnection - number of participants endorsing this theme
-disconnect %>%
+dis_n <- disconnect %>%
   select(content) %>%
   filter(!is.na(content)) %>%
   nrow()
+dis_n
+
+# Percentages
+(saf_n / 1272) * 100
+(con_n / 1272) * 100
+(env_n / 1272) * 100
+(ide_n / 1272) * 100
+(dis_n / 1272) * 100
